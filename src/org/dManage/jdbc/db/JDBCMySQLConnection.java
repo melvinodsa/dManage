b04a93ec528636611d8beece5cf2c069ba027d74
Melvin Davis <melvinodsa@gmail.com>
2015-06-13 22:17:18 +0530

    initiated jdbc

---
+++ src/org/dManage/jdbc/db/JDBCMySQLConnection.java
@@ -0,0 +1,38 @@
+package org.dManage.jdbc.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JDBCMySQLConnection {
+	private static JDBCMySQLConnection instance = new JDBCMySQLConnection();
+	public static final String URL = "jdbc:mysql://localhost:3306/userlist";
+	public static final String USER = "testuser";
+	public static final String PASSWORD = "qp";
+	public static final String DRIVER_CLASS = "com.mysql.jdbc.Driver";
+	
+	public JDBCMySQLConnection() {
+		try {
+			//Step 2: Load MySQL Java driver
+			Class.forName(DRIVER_CLASS);
+		} catch (ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private Connection createConnection() {
+
+		Connection connection = null;
+		try {
+			//Step 3: Establish Java MySQL connection
+			connection = DriverManager.getConnection(URL, USER, PASSWORD);
+		} catch (SQLException e) {
+			System.out.println("ERROR: Unable to Connect to Database.");
+		}
+		return connection;
+	}
+	
+	public static Connection getConnection() {
+		return instance.createConnection();
+	}
+}
