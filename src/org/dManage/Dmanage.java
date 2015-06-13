0c125d9ac682a60e2762e4ebeb32e0ac891966fd
Melvin Davis <melvinodsa@gmail.com>
2015-06-13 22:14:53 +0530

    initiated jdbc

--- src/org/dManage/Dmanage.java
+++ src/org/dManage/Dmanage.java
@@ -2,6 +2,10 @@ package org.dManage;
 
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
@@ -9,12 +13,33 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.dManage.jdbc.db.JDBCMySQLConnection;
+import org.dManage.jdbc.db.User;
+
 /**
  * Servlet implementation class Dmanage
  */
 @WebServlet(description = "simple servlet", urlPatterns = { "/Dmanage" })
 public class Dmanage extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+		
+	        response.setContentType("text/html;charset=UTF-8");
+	        PrintWriter out = response.getWriter();
+		
+	        String id = request.getParameter("id");
+		try {
+			int employeeId = Integer.parseInt(id);
+			Dmanage demo = new Dmanage();
+			User employee = demo.getEmployee(employeeId);
+			out.println(employee);			
+		} catch (NumberFormatException e) {
+			e.printStackTrace();
+		}
+	}
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
@@ -22,7 +47,43 @@ public class Dmanage extends HttpServlet {
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		System.out.println("Hello from GET method");
 		PrintWriter writer = response.getWriter();
-		writer.println("<html><h3>Hello me again. But I am <em>dynamic</em> now</h3></html>");
+		writer.println("<html><form method=\"post\" action=\"Dmanage\">Id:<input type=\"text\""+
+		"name=\"id\" /><br/><input type=\"submit\" value=\"register\" />"+
+		"</form></html>");
+	}
+
+
+	private User getEmployee(int id) {
+		ResultSet rs = null;
+		Connection connection = null;
+		Statement statement = null; 
+		
+		User user = null;
+		String query = "SELECT * FROM users WHERE id=" + id;
+		try {			
+			connection = JDBCMySQLConnection.getConnection();
+			statement = connection.createStatement();
+			rs = statement.executeQuery(query);
+			
+			if (rs.next()) {
+				user = new User();
+				user.setId(rs.getInt("id"));
+				user.setName(rs.getString("name"));
+				user.setEmail(rs.getString("email"));
+				user.setPassword((rs.getString("password")));
+			}
+		} catch (SQLException e) {
+			e.printStackTrace();
+		} finally {
+			if (connection != null) {
+				try {
+					connection.close();
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+			}
+		}
+		return user;
 	}
 
 }
